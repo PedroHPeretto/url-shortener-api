d832dac65699fafb711eb163830abdaa2ccebea1
PedroPeretto-FHC <pedro.peretto@fcbhealth.com>
2025-11-13 12:25:56 -0300

    fix(auth, docker): fixing linting issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
+        port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USER'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
