45e795eee61c04a3be77c344f39efefc70b936ea
PedroPeretto-FHC <pedro.peretto@fcbhealth.com>
2025-11-14 11:16:30 -0300

    chore(urls): creating urls endpoints service and dto

--- src/urls/dto/create-url.dto.ts
+++ src/urls/dto/create-url.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUrlDto {}
+import { IsNotEmpty, IsUrl } from 'class-validator';
+
+export class CreateUrlDto {
+  @IsNotEmpty({ message: 'A url não pode ser vazia.' })
+  @IsUrl({}, { message: 'A url fornecida é invalida.' })
+  original_url: string;
+}
--- src/urls/urls.service.ts
+++ src/urls/urls.service.ts
@@ -1,26 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Url } from './entities/url.entity';
 import { CreateUrlDto } from './dto/create-url.dto';
-import { UpdateUrlDto } from './dto/update-url.dto';
+import { nanoid } from 'nanoid';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class UrlsService {
-  create(createUrlDto: CreateUrlDto) {
-    return 'This action adds a new url';
+  private readonly logger = new Logger(UrlsService.name);
+
+  constructor(
+    @InjectRepository(Url)
+    private readonly urlRepository: Repository<Url>,
+  ) {}
+
+  async generateUniqueShortCode(): Promise<string> {
+    const code = nanoid(6);
+    const existing = await this.urlRepository.findOneBy({ short_code: code });
+
+    if (existing) {
+      this.logger.warn(`Colisão de short codes detectada: ${code}`);
+    }
+
+    return code;
   }
 
-  findAll() {
-    return `This action returns all urls`;
+  async shortenUrl(createUrlDto: CreateUrlDto, user?: User) {
+    const { original_url } = createUrlDto;
+
+    const short_code = await this.generateUniqueShortCode();
+
+    const newUrl = this.urlRepository.create({
+      original_url,
+      short_code,
+      user: user,
+    });
+
+    await this.urlRepository.save(newUrl);
+
+    return {
+      ...newUrl,
+      short_url: `http://localhost:3000/${short_code}`,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} url`;
-  }
+  async findOriginalUrlAndCountClick(short_code: string) {
+    const url = await this.urlRepository.findOneBy({
+      short_code,
+    });
 
-  update(id: number, updateUrlDto: UpdateUrlDto) {
-    return `This action updates a #${id} url`;
-  }
+    if (!url) {
+      return null;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} url`;
+    void this.urlRepository
+      .increment({ id: url.id }, 'click_count', 1)
+      .catch((err) => {
+        this.logger.error('Falha ao contabilizar clique:', err);
+      });
+
+    return url.original_url;
   }
 }
