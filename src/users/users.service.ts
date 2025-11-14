3b280b3c57a8945f94cb6f2584a50b6717afac81
PedroPeretto-FHC <pedro.peretto@fcbhealth.com>
2025-11-14 17:16:05 -0300

    fix(users): fixing unhashed password on service function update user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { UserResponse } from './interfaces/user-response.type';
 
 @Injectable()
 export class UsersService {
@@ -21,17 +23,31 @@ export class UsersService {
     return this.userRepository.findOne({ where: { email } });
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+  async update(
+    id: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserResponse> {
+    const updateData = { ...updateUserDto };
+    if (updateData.password) {
+      const saltRounds = 10;
+
+      updateData.password = await bcrypt.hash(updateData.password, saltRounds);
+    }
+
     const user = await this.userRepository.preload({
       id: id,
-      ...updateUserDto,
+      ...updateData,
     });
 
     if (!user) {
       throw new NotFoundException('Usuário não encontrado');
     }
 
-    return await this.userRepository.save(user);
+    const savedUser = await this.userRepository.save(user);
+
+    const { password: _, ...result } = savedUser;
+
+    return result;
   }
 
   async remove(id: string): Promise<void> {
