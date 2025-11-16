e81fe2f8be5d36be186ccba41a5c7208b4bf9a8e
PedroPeretto-FHC <pedro.peretto@fcbhealth.com>
2025-11-16 13:16:40 -0300

    fix(users): removing unused endpoint create users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,5 @@
-import {
-  Controller,
-  Post,
-  Body,
-  Patch,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Body, Patch, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { GetUser } from '../auth/decorators/get-user.decorator';
 import { User } from './entities/user.entity';
@@ -16,11 +8,6 @@ import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
 
   @UseGuards(JwtAuthGuard)
   @Patch()
