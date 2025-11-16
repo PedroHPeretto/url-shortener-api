60fbb8720e02c0c6562e5c1b0f040d5b373e2299
PedroPeretto-FHC <pedro.peretto@fcbhealth.com>
2025-11-16 20:34:25 -0300

    chore(auth): improving validation pipes at auth dtos

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength, Matches } from 'class-validator';
 
 export class LoginDto {
   @IsNotEmpty({ message: 'O e-mail não pode ser vazio.' })
@@ -6,5 +6,9 @@ export class LoginDto {
   email: string;
 
   @IsNotEmpty({ message: 'A senha não pode ser vazia.' })
+  @MinLength(6, { message: 'Password must contain at least 6 characters.' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'A senha deve conter letras maiúsculas, minúsculas e números',
+  })
   password: string;
 }
